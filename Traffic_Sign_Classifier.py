4e0ac7b5df3ccaa2e8729b8d84370605eb212727
Yonatan Mittlefehldt <yono@toojuice.com>
2017-02-17 21:49:13 +0100

    Put back initial reading in of the training data. Simplifies things for the wrapper.

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -37,14 +37,14 @@ training_pp_file = "../data/traffic-signs-data/train_pp.p"
 ### Preprocessed balanced file
 training_pp_bal_file = "../data/traffic-signs-data/train_pp_bal.p"
 
-#with open(training_file, mode='rb') as f:
-#    train = pickle.load(f)
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
 with open(validation_file, mode='rb') as f:
     valid = pickle.load(f)
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
 
-#X_train, y_train = train['features'], train['labels']
+X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
