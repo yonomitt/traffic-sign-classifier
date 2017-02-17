2321cab0e6be55fce6f18b578f52b4515b6d5047
Yonatan Mittlefehldt <yono@toojuice.com>
2017-02-17 21:53:11 +0100

    Added support to preprocess data in the wrapper script

--- project2.py
+++ project2.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     dummy = parser.add_argument('-e', '--epochs', type=int, help='Number of epochs to train', default=20)
     dummy = parser.add_argument('-k', '--keep_prob', type=float, help='Keep probability when using dropout', default=1.0)
     dummy = parser.add_argument('-t', '--training_set', type=str, help='Training set to use', default='../data/traffic-signs-data/train.p')
+    dummy = parser.add_argument('-p', '--preprocess', type=str, help='Preprocess data with this function', default='')
 
     args = parser.parse_args()
 
@@ -28,6 +29,10 @@ if __name__ == '__main__':
 
     if not args.network in dir():
         print("Network '{}' NOT FOUND".format(args.network))
+        sys.exit(-1)
+
+    if args.preprocess and not args.preprocess in dir():
+        print("Preprocess function '{}' NOT FOUND".format(args.preprocess))
         sys.exit(-1)
 
     with open(args.training_set, mode='rb') as f:
@@ -43,6 +48,10 @@ if __name__ == '__main__':
 
     if training_id:
         model_id_parts.append(training_id)
+
+    if args.preprocess:
+        model_id_parts.append(args.preprocess)
+        globals()[args.preprocess]()
 
     model_id_parts.append('e{}'.format(args.epochs))
 
