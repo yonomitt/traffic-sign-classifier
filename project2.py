f498642b53cb96c3cfeaa2a61d6005a67c13a8b3
Yonatan Mittlefehldt <yono@toojuice.com>
2017-02-17 09:07:21 +0100

    Created a wrapper script to facilitate automatically training networks with different parameters

---
+++ project2.py
@@ -0,0 +1,51 @@
+
+import argparse
+import sys
+
+from Traffic_Sign_Classifier import *
+
+
+class ArgParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('ERROR: %s\n\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+
+if __name__ == '__main__':
+
+    parser = ArgParser(description='Trains a traffic sign classifier', add_help=False)
+    dummy = parser.add_argument('-n', '--network', type=str, help='Name of the network to run', default='LeNet')
+    dummy = parser.add_argument('-e', '--epochs', type=int, help='Number of epochs to train', default=20)
+    dummy = parser.add_argument('-k', '--keep_prob', type=float, help='Keep probability when using dropout', default=1.0)
+    dummy = parser.add_argument('-t', '--training_set', type=str, help='Training set to use', default='../data/traffic-signs-data/train.p')
+
+    args = parser.parse_args()
+
+    if not args.network in dir():
+        sys.exit(-1)
+
+    with open(args.training_set, mode='rb') as f:
+        train = pickle.load(f)
+
+    X_train, y_train = train['features'], train['labels']
+
+    n_examples = len(X_train)
+
+    # build model identifier
+    model_id_parts = [args.network]
+    training_id = args.training_set.split('/')[-1].split('.')[0].replace('train_', '').replace('train', '')
+
+    if training_id:
+        model_id_parts.append(training_id)
+
+    model_id_parts.append('e{}'.format(args.epochs))
+
+    keep_prob_val = max(min(args.keep_prob, 1.0), 0.01)
+
+    if 'dropout' in args.network:
+        model_id_parts.append('k{}'.format(keep_prob_val))
+        train_network(globals()[args.network](x, keep_prob), '_'.join(model_id_parts), epochs=args.epochs, keep_prob_val=keep_prob_val)
+    else:
+        train_network(globals()[args.network](x), '_'.join(model_id_parts), epochs=args.epochs)
+
